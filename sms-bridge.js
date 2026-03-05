33e50a427ad133b93495c4a36ab93d8ce86f97f3
Amit Singh <amitedge69@gmail.com>
2026-03-06 00:21:10 +0530

    initial sms bridge

--- sms-bridge.js
+++ sms-bridge.js
@@ -6,30 +6,34 @@ const app = express();
 const FAST2SMS_KEY = process.env.FAST2SMS_KEY;
 
 app.get("/http-api.php", async (req, res) => {
-  const { username, password, sender, to, message } = req.query;
+  const { sender, to, message } = req.query;
 
   try {
-    const response = await axios.get("https://www.fast2sms.com/dev/bulkV2", {
-      params: {
-        authorization: FAST2SMS_KEY,
-        route: "dlt",
-        sender_id: sender,
+    const response = await axios.post(
+      "https://www.fast2sms.com/dev/bulkV2",
+      {
+        route: "q",
         numbers: to,
         message: message,
+        language: "english",
       },
-    });
+      {
+        headers: {
+          authorization: FAST2SMS_KEY,
+          "Content-Type": "application/json",
+        },
+      },
+    );
 
     res.json(response.data);
   } catch (error) {
     res.status(500).json({
       error: "SMS sending failed",
-      details: error.message,
+      details: error.response?.data || error.message,
     });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("SMS Bridge running on port " + PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("SMS Bridge running");
 });
